af1475607e3dfb76365151b68d987b01a80db9a7
ziedtuihri <zied.hack@gmail.com>
2020-09-27 03:22:36 +0100

    Spring Initializr

--- springweb/src/test/java/com/example/springweb/SpringwebApplicationTests.java
+++ springweb/src/test/java/com/example/springweb/SpringwebApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.springweb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class SpringwebApplicationTests {
 
 	@Test
 	void contextLoads() {
