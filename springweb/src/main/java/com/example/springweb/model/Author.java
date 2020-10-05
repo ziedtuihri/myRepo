3908ebc10ce6cffe9bf2f0aa950f9de6e5574bea
ziedtuihri <zied.hack@gmail.com>
2020-10-05 23:35:00 +0100

    JPA model

---
+++ springweb/src/main/java/com/example/springweb/model/Author.java
@@ -0,0 +1,38 @@
+package com.example.springweb;
+
+
+public class Author {
+/*
+ * @Author: zied
+ * @created: 04/10/2020
+ */
+
+    private String firstName;
+    private String lastName;
+
+    public Author(){
+
+    }
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+}
---
+++ springweb/src/main/java/com/example/springweb/model/Book.java
@@ -0,0 +1,5 @@
+package com.example.springweb;
+
+public class Book {
+
+}
